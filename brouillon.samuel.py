f3e7f18a732b0d9720641517e33b3760ea1a366f
sameueeel <91207987+sameueeel@users.noreply.github.com>
2022-02-07 18:03:17 +0100

    Create brouillon.samuel.py

---
+++ brouillon.samuel.py
@@ -0,0 +1,4 @@
+## comment integrer les diff IA dans la fonction clique
+#une colonne joueur 1 ou on choisit joueur 1 parmis des IA ou joueur physique 
+#.......joueur2.........joueur 2
+#il faut donc faire une fonction click qui s'active au bout de 2 click 
