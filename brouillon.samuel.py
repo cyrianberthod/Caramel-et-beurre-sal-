2e900899e6755a0bb8d38588089515571c2a3aa0
sameueeel <91207987+sameueeel@users.noreply.github.com>
2022-02-27 20:03:58 +0100

    Update brouillon.samuel.py

--- brouillon.samuel.py
+++ brouillon.samuel.py
@@ -2,3 +2,5 @@
 #une colonne joueur 1 ou on choisit joueur 1 parmis des IA ou joueur physique 
 #.......joueur2.........joueur 2
 #il faut donc faire une fonction click qui s'active au bout de 2 click 
+#il faut passer par des classes pour faire cohabiter IA voir lien ci dessous pour comprendre
+#https://kongakura.fr/article?id=Cr%C3%A9er_une_I.A_qui_apprend_toute_seule_%C3%A0_jouer_au_morpion
